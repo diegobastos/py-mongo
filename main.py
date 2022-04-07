a1322f3714fc73077bec476c0a8d43a79da78f69
Diego <diegobastos7@gmail.com>
2022-04-06 21:02:02 -0400

    Adição de inserção múltipla e pesquisa de retorno único e múltiplo

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # 2 instalar dnspython para acesso ao host do banco: python -m pip install dnspython
 # 3 cria banco mongo utilizando o script:
 from pymongo import MongoClient
+import pprint
 
 # função em python é declarada utilizando a key word def : define
 # vai conectar no banco mongo que for passado como parâmetro
@@ -15,21 +16,55 @@ def get_database(database):
 def insert_farm(farm): # insere uma nova fazenda
     db = get_database("class_19004") # conectando no banco e salvando a conexao na var db
     #     con.collection.funcao
-    ret = db.farms.insert_one(farm)
+    ret = db.farms.insert_one(farm) # inserindo um documento
+    # 2a forma de conectar-se a uma collection
+    # ret = db['farms'].insert_one(farm)
     return ret.inserted_id
+
+def insert_farms(farms):
+    db = get_database("class_19004")
+    ret = db.farms.insert_many(farms) # inserindo vários documentos
+    return ret.inserted_ids # retornando os últimos ids inseridos (ids dos docs)
+
+def find_one_farm(conditions):
+    # pprint.pprint( get_database("class_19004").farms.find(conditions)  )
+    db = get_database("class_19004")
+    col = db.farms
+    pprint.pprint(col.find_one(conditions))
+
+def find_farms(conditions):
+    col = get_database("class_19004").farms
+    #   var  |-lista------------|
+    for f in col.find(conditions):
+        pprint.pprint(f)
 
 # bloco que inicia um programa em python
 if __name__ == "__main__":
     #python listas, tuplas, dicionários
     #mongo JSONs { "key": "value" }
-
     #dictionary in python newFarm
-    newFarm ={
-        "name": "Farm's Python 1",
-        "manager": "João Quebra Toco"
+    farm1 ={
+        "name": "Two Brothers",
+        "manager": "Big Lauro"
     }
+
+    farm2={
+        "name": "Big Farm",
+        "manager": "Crzy Paul"
+    }
+
+    farm3={
+        "name": "Big Farm Three",
+        "manager": "Crzy Paul"
+    }
+
     # arm. na var id o último id inserido
-    id = insert_farm(newFarm)
+    #id = insert_farm(farm1)
+    #ids = insert_farms([farm2, farm3]) # lista de docs -> [d1, d2, d3, ...]
 
     #imprimindo a mensagem com o ID da fazenda gerado pelo mongoDB
-    print(f"Fazenda inserida. ID:[ {id} ]")
+    #print(f"Fazenda inserida. ID:[ {id} ]")
+    #print(f"Fazendas inseridas. Ids:{ ids }")
+
+    #find_one_farm({"manager": "Big Lauro"})
+    find_farms({"manager":"João Quebra Toco"})
